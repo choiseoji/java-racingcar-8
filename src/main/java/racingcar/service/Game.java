c479213b3fd97a6216159b501ad6d31750ee975e
choiseoji <choiseoji123@naver.com>
2025-10-25 20:33:28 +0900

    feat: 게임 진행하는 함수 추가

--- src/main/java/racingcar/service/Game.java
+++ src/main/java/racingcar/service/Game.java
@@ -1,6 +1,13 @@
 package racingcar.service;
 
+import racingcar.dto.CarStatus;
+import racingcar.dto.FinalResult;
+import racingcar.dto.RoundResult;
+import racingcar.model.Car;
 import racingcar.model.Cars;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
 
@@ -17,5 +24,30 @@ public class Game {
         if (round <= 0) {
             throw new IllegalArgumentException("라운드 수는 1 이상이어야 합니다.");
         }
+    }
+
+    public FinalResult play() {
+        List<RoundResult> roundResults = new ArrayList<>();
+
+        for (int i = 0; i < round; i++) {
+            RoundResult roundResult = playOneRound();
+            roundResults.add(roundResult);
+        }
+        List<String> winnerNames = cars.getWinnerNames();
+
+        return new FinalResult(roundResults, winnerNames);
+    }
+
+    private RoundResult playOneRound() {
+        List<CarStatus> carStatuses = new ArrayList<>();
+
+        for(Car car : this.cars.getCars()) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            if (randomNumber >= 4) {
+                car.increaseMoveCount();
+            }
+            carStatuses.add(new CarStatus(car.getName(), car.getMoveCount()));
+        }
+        return new RoundResult(carStatuses);
     }
 }
