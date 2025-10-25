debd331953918b9863297ecdb276a6db8938adcb
choiseoji <choiseoji123@naver.com>
2025-10-25 18:30:45 +0900

    feat: input에서 Cars 만드는 from 함수 추가

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,43 @@
+package racingcar.model;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Cars {
+
+    private List<Car> cars;
+
+    private Cars(List<Car> cars) {
+        this.cars = List.copyOf(cars);
+    }
+
+    public static Cars from(String input) {
+        validateInput(input);
+
+        String[] names = input.split(",");
+        List<Car> cars = Arrays.stream(names)
+                .map(Car::new)
+                .toList();
+
+        validateDuplicate(cars);
+        return new Cars(cars);
+    }
+
+    private static void validateInput(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
+        }
+    }
+
+    private static void validateDuplicate(List<Car> cars) {
+        long uniqueCount = cars.stream()
+                .map(Car::getName)
+                .distinct()
+                .count();
+
+        if (uniqueCount != cars.size()) {
+            throw new IllegalArgumentException("중복된 자동차 이름이 있습니다.");
+        }
+    }
+}
