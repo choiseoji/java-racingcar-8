8671ddb0a0bcb465b3cfbdc6b1823e9b894e4bc5
choiseoji <choiseoji123@naver.com>
2025-10-25 11:46:36 +0900

    feat: name을 입력받는 Car 생성자 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+public class Car {
+
+    private String name;
+    private Integer moveCount;
+
+    public Car(String name) {
+        this.name = name;
+        this.moveCount = 0;
+    }
+}
