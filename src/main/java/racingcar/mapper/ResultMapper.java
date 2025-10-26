96020cdb77bad507e2555a9a5601e8179d8acba3
choiseoji <choiseoji123@naver.com>
2025-10-26 10:27:25 +0900

    feat: DTO로 변환하는 mapper 클래스 추가

---
+++ src/main/java/racingcar/mapper/ResultMapper.java
@@ -0,0 +1,30 @@
+package racingcar.mapper;
+
+import racingcar.dto.CarStatus;
+import racingcar.dto.FinalResult;
+import racingcar.dto.RoundResult;
+import racingcar.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResultMapper {
+
+    public FinalResult toFinalResult(List<List<Car>> roundData, List<String> winners) {
+
+        List<RoundResult> roundResults = new ArrayList<>();
+        for(List<Car> cars : roundData) {
+            roundResults.add(toRoundResult(cars));
+        }
+        return new FinalResult(roundResults, winners);
+    }
+
+    public RoundResult toRoundResult(List<Car> cars) {
+
+        List<CarStatus> carStatuses = new ArrayList<>();
+        for(Car car : cars) {
+            carStatuses.add(new CarStatus(car.getName(), car.getMoveCount()));
+        }
+        return new RoundResult(carStatuses);
+    }
+}
