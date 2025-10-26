3bb24dc2e82d85b29e5e1a00167fb5be0d963546
choiseoji <choiseoji123@naver.com>
2025-10-26 17:16:57 +0900

    refact: FinalResult record 타입으로 변경

--- src/main/java/racingcar/dto/FinalResult.java
+++ src/main/java/racingcar/dto/FinalResult.java
@@ -2,21 +2,8 @@ package racingcar.dto;
 
 import java.util.List;
 
-public class FinalResult {
-
-    List<RoundResult> roundResults;
-    List<String> winners;
-
-    public FinalResult(List<RoundResult> roundResults, List<String> winners) {
-        this.roundResults = List.copyOf(roundResults);
-        this.winners = List.copyOf(winners);
-    }
-
-    public List<RoundResult> getRoundResults() {
-        return this.roundResults;
-    }
-
-    public List<String> getWinners() {
-        return this.winners;
-    }
+public record FinalResult(
+        List<RoundResult> roundResults,
+        List<String> winners
+) {
 }
