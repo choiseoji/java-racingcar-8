6e23d422059cbbef2bfb5c05dc0e09221a00ef5d
choiseoji <choiseoji123@naver.com>
2025-10-26 17:16:31 +0900

    refact: RoundResult record 타입으로 변경

--- src/main/java/racingcar/dto/RoundResult.java
+++ src/main/java/racingcar/dto/RoundResult.java
@@ -2,14 +2,7 @@ package racingcar.dto;
 
 import java.util.List;
 
-public class RoundResult {
-    List<CarStatus> carStatuses;
-
-    public RoundResult(List<CarStatus> cars) {
-        this.carStatuses = List.copyOf(cars);
-    }
-
-    public List<CarStatus> getCarStatuses() {
-        return this.carStatuses;
-    }
+public record RoundResult(
+        List<CarStatus> carStatuses
+) {
 }
